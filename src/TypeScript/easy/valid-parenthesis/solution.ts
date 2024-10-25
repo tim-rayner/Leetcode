99f7c94655a478d88ef65bfc55c3007136566ef9
Tim Rayner <tim.rayner@liftshare.com>
2024-10-25 16:54:34 +0100

    solution: implemented isValidParenthesis solution

---
+++ src/TypeScript/easy/valid-parenthesis/solution.ts
@@ -0,0 +1,74 @@
+const input = "(test)";
+
+export function isValidParenthesis(input: string): boolean {
+  //split string into array
+  const inputArr = input.split("");
+
+  //create a LIFO stack
+  const stack = [];
+
+  //loop through inputArr
+  for (let i = 0; i < inputArr.length; i++) {
+    console.log(inputArr[i]);
+    const currentChar = inputArr[i];
+    //if the character is an opening bracket, push it onto the stack
+    if (isOpening(currentChar)) {
+      stack.push(currentChar);
+    }
+
+    if (isClosing(currentChar)) {
+      if (stack.length === 0) return false;
+
+      const lastStackChar = stack.pop();
+
+      //check popped char matches current char in terms of parenthesis
+      if (!checkMatching(lastStackChar!, currentChar)) {
+        return false;
+      }
+    }
+  }
+
+  //check stack is empty
+  return stack.length === 0;
+}
+
+function isOpening(inputChar: string): boolean {
+  switch (inputChar) {
+    case "(":
+      return true;
+    case "{":
+      return true;
+    case "[":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function isClosing(inputChar: string): boolean {
+  switch (inputChar) {
+    case ")":
+      return true;
+    case "}":
+      return true;
+    case "]":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function checkMatching(openingChar: string, currentChar: string): boolean {
+  switch (openingChar) {
+    case "(":
+      return currentChar === ")";
+    case "{":
+      return currentChar === "}";
+    case "[":
+      return currentChar === "]";
+    default:
+      return false;
+  }
+}
+
+console.log(isValidParenthesis(input));
